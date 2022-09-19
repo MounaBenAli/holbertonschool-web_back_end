9f56c18517f1542415912ce60cb1ed6bd4195a45
MounaBenAli <benalimouna1986@gmail.com>
2022-09-19 18:49:43 +0200

    0. Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,8 +3,10 @@
 
 from typing import *
 
+
 def add(a: float, b: float) -> float:
     """
-    Takes a float a and a float b as arguments and returns their sum as a float.
+    Takes a float a and a float b as arguments
+    and returns their sum as a float.
     """
     return (a + b)
