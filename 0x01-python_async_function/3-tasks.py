79c48f78813a093682b7acf95f539f620ec64863
MounaBenAli <benalimouna1986@gmail.com>
2022-09-20 14:40:12 +0200

    3. Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Tasks """
+
+import asyncio
+from asyncio import Task
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """a function task_wait_random that takes
+       an integer max_delay.
+       Returns a asyncio.Task.
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    3return (task)
