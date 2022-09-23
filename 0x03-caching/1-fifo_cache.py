c59d1e1104389531488ea77a089d87cf525584fb
MounaBenAli <benalimouna1986@gmail.com>
2022-09-23 17:36:31 +0200

    1. FIFO caching

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -20,7 +20,7 @@ class FIFOCache(BaseCaching):
            discard the first item put in cache (FIFO algorithm)
         """
         self.cache_data[key] = item
-        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS):
             first_key = next(iter(self.cache_data))
             print("DISCARD:", first_key)
             del self.cache_data[first_key]
