504ff510f699d2695ceec68b0110a4728ca9acc4
MounaBenAli <benalimouna1986@gmail.com>
2022-09-23 19:00:07 +0200

    2. LIFO Caching

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""  LIFO caching """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """child class inherits from parent BaseCaching
+        BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """Assigns to the dictionary self.cache_data the values for the keys
+           If the number of items in self.cache_data > BaseCaching.MAX_ITEMS:
+           discard the last item put in cache (LIFO algorithm)
+        """
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if key in self.cache_data.keys():
+                last_key = list(self.cache_data)[-2]
+                print("DISCARD:", last_key)
+                del self.cache_data[last_key]
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        else:
+            return self.cache_data[key]
