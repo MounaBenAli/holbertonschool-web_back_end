60340eafb654bb45bfe367b921f44ee030a7af6e
MounaBenAli <benalimouna1986@gmail.com>
2022-10-12 17:02:10 +0200

    Request validation

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -3,14 +3,14 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views import app_views
+# from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+# app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
