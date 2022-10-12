6c01d4ec46944999e6b6f8f0e7adb1848381ee3b
MounaBenAli <benalimouna1986@gmail.com>
2022-10-12 13:32:52 +0200

    Handle path authentication

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,17 @@ class Auth:
     """ Auth class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False - path and excluded_paths """
-        return False
+        """handle path authentication"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """returns None - request"""
