bc98094259d95276e6fcb416710f031a8b5b894f
MounaBenAli <benalimouna1986@gmail.com>
2022-10-12 07:15:02 +0200

    fix starter api files

--- 0x06-Basic_authentication/api/v1/views/users.py
+++ 0x06-Basic_authentication/api/v1/views/users.py
@@ -3,7 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
-from models.user import User
+#from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
