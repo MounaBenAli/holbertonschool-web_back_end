5b25acb6170d77b1a6039bad0bd17d77ee3d0362
MounaBenAli <benalimouna1986@gmail.com>
2022-10-13 15:38:51 +0200

    1. create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""DB module
+"""
+from curses.ascii import US
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db")
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """returns a new User object and saves it to the DB"""
+        new = User(email=email, hashed_password=hashed_password)
+        self._session.add(new)
+        self._session.commit()
+        return new
