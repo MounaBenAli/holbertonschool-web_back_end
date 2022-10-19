2397142dafe8359177cb308ea96963c75a748ceb
MounaBenAli <benalimouna1986@gmail.com>
2022-10-19 20:04:48 +0200

    7. Parameterize

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -51,7 +51,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
-    ])
+        ])
     def test_has_license(self, repo, license_key, result):
         """unit-test GithubOrgClient.has_license"""
         test = GithubOrgClient('test')
