bd47e2d74d3c22a1aaf99da783e5e5bed69d9049
MounaBenAli <benalimouna1986@gmail.com>
2022-10-20 13:32:40 +0200

    0. Basic Flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
-
+""" Basic Flask app """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     """Renders html template"""
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
