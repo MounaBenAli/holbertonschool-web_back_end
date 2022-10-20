f817f1cc398063da445042a6db62b5cd492ca02e
MounaBenAli <benalimouna1986@gmail.com>
2022-10-20 13:48:44 +0200

    1. Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ configures available languages in the app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Renders html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
