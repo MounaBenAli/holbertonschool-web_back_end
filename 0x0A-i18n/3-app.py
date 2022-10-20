9201ca4e81d06b72f0255d322714c5bbb11816c6
MounaBenAli <benalimouna1986@gmail.com>
2022-10-20 22:57:48 +0200

    3. Parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Parametrize templates"""
 from flask import Flask, render_template
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 from flask import request
 
 app = Flask(__name__)
