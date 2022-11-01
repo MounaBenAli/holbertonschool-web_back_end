5e150109e8e1e7134e929f966aa1186b83196852
MounaBenAli <benalimouna1986@gmail.com>
2022-11-01 10:14:52 +0100

    1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,6 +7,14 @@ from pytest import param
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Increments the count for that key every time the method is called
+        & Returns the value returned by the original method.
+    """
+    __qualname__ = key
 
 
 class Cache():
@@ -26,9 +34,9 @@ class Cache():
 
     def get(self, key: str, fn: Optional[Callable]
             ) -> Union[str, bytes, int, float]:
-        """Fetch data from redis"""
+        """Fetch data from redis cache"""
         data = self._redis.get(key)
-        if not callable(fn):
+        if fn:
             return data
         else:
             return fn(data)
