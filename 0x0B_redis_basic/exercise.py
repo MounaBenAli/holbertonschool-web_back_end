25558dc4d3b571b4df7d6eb27ed59ce32bff4900
MounaBenAli <benalimouna1986@gmail.com>
2022-11-01 09:14:57 +0100

    1. Reading from Redis and recovering original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,11 @@
 """
 Learning Redis
 """
+from matplotlib.backend_bases import key_press_handler
+from pytest import param
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -16,8 +18,25 @@ class Cache():
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Generates a random key and store it in redis"""
+        """Stores data in redis cache in a key-value pair"""
         k = str(uuid.uuid4())
         self._redis.mset({k: data})
 
         return k
+
+    def get(self, key: str, fn: Optional[Callable]
+            ) -> Union[str, bytes, int, float]:
+        """Fetch data from redis"""
+        data = self._redis.get(key)
+        if not callable(fn):
+            return data
+        else:
+            return fn(data)
+
+    def get_str(self, key: str) -> str:
+        """returns the key as an str value"""
+        return str(self._redis.get(key), "UTF-8")
+
+    def get_int(self, key: int) -> int:
+        """returns the key as an int value"""
+        return int(self._redis.get(key))
