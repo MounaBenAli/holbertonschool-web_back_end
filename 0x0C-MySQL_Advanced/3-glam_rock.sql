84a76cc2c4f5ae2efb9f57ff82c90a784ec5a5be
MounaBenAli <benalimouna1986@gmail.com>
2022-11-03 17:16:32 +0100

    3. Old school band

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -5,4 +5,8 @@
 -- Column names must be: band_name and lifespan (in years)
 -- You should use attributes formed and split for computing the lifespan
 -- Your script can be executed on any database
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan FROM metal_bands WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
+
+SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan 
+    FROM metal_bands
+    WHERE style LIKE "%Glam rock%"
+    ORDER BY lifespan DESC;
