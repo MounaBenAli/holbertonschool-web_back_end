3b489f2d1e0ba0528bb842600e77dcbfcb193395
MounaBenAli <benalimouna1986@gmail.com>
2022-11-03 16:58:09 +0100

    6. Add bonus

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -14,7 +14,7 @@ BEGIN
     THEN INSERT INTO projects (name) VALUES (project_name);
     END IF;
 
-    SET @pro_id = (SELECT id FROM projects WHERE name = project_name);
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, @pro_id, score);
-END $$
+    INSERT INTO corrections (user_id, project_id, score) 
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
+END;$$
 DELIMITER ;
