366f4884d1efbda533424d315282dcd8f635e696
MounaBenAli <benalimouna1986@gmail.com>
2022-11-07 16:17:59 +0100

    8. List all documents in Python

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -3,7 +3,7 @@
 
 
 def list_all(mongo_collection):
-    """ method to lists all documents in a collection"""
+    """ method to list all documents in a collection"""
     if mongo_collection is None:
         return []
     return mongo_collection.find()
