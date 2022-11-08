05b439638f6e6b29a14b44a328e62655cd193eb8
MounaBenAli <benalimouna1986@gmail.com>
2022-11-08 11:28:26 +0100

    11. Where can I learn Python?

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-""" Sort a document """
+""" Find an element inside a document """
 
 
 def schools_by_topic(mongo_collection, topic):
     """ method that returns the list of school having a specific topic"""
-    return mongo_collection.find_one({"topic": topic})
+    return mongo_collection.find({ "topics": topic })
+
