bf5b9670b6c07ca1aaeab095acb89f8cf5c6fbba
MounaBenAli <benalimouna1986@gmail.com>
2022-11-15 11:53:42 +0100

    3. Handle multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -6,8 +6,7 @@ const promise2 = createUser();
 function handleProfileSignup() {
   Promise.all([promise1, promise2])
     .then((values) => console.log(values[0].body, values[1].firstName, values[1].lastName))
-    .catch(() => new Error('Signup system offline'))
-    .finally(() => {});
+    .catch(() => new Error('Signup system offline'));
 }
 
 export default handleProfileSignup;
