9a8f67913654ee0600c02314a554c0c642535bf5
MounaBenAli <benalimouna1986@gmail.com>
2022-11-15 14:05:36 +0100

    6. Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
   const promises = [promise1, promise2];
-  return Promise.allSettled(promises).then(() => {
+  return Promise.all(promises).then(() => {
   }).catch(() => {
   }).finally((results) => results);
 }
