deae7eb3a9dea7c0562adb07e29f252e43539ef2
MounaBenAli <benalimouna1986@gmail.com>
2022-11-15 13:44:40 +0100

    6. Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
+/* eslint no-unused-vars: "error" */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const promise1 = signUpUser();
-const promise2 = uploadPhoto();
-const promises = [promise1, promise2];
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+  const promises = [promise1, promise2];
   return Promise.allSettled(promises)
     .then((results) => results.forEach((result) => {
       console.log(
