bccc724b643fdf0119a0a41bdca16c237cd8f405
MounaBenAli <benalimouna1986@gmail.com>
2022-11-15 10:30:48 +0100

    2. Catch me if you can

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,13 +1,8 @@
 function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    // executor
-    console.log('Got a response from the API');
-    if (promise) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error());
-    }
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
 
 export default handleResponseFromAPI;
