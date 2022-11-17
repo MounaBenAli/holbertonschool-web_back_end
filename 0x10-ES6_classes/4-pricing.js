dd2942a8efceea8d491a0889624a3a270139dc6d
MounaBenAli <benalimouna1986@gmail.com>
2022-11-17 13:15:51 +0100

    4. Pricing

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount; // string
+    this.currency = currency; // string
+  }
+
+  // Setters & Getters for the attribute amount
+  set amount(Amount) {
+    this._amount = Amount;
+  }
+
+  get amount() { return this._amount; }
+
+  // Setters & Getters for the attribute currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  get currency() { return this._currency; }
+
+  // method displayFullPrice() Implementation
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  // static method convertPrice() Implementation
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
