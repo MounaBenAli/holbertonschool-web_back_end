dc6b30d1f4ec37606c19b9dd6342f646875554d2
MounaBenAli <benalimouna1986@gmail.com>
2022-11-21 14:35:15 +0100

    0. Basic list of objects

--- 0x11-ES6_data_manipulation/0-get_list_students.js
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,20 @@
 export default function getListStudents() {
   const arr = [
     {
-      id: '1',
+      id: 1,
       firstName: 'Guillaume',
       location: 'San Francisco',
     },
     {
-      id: '2',
+      id: 2,
       firstName: 'James',
       location: 'Columbia',
     },
     {
-      id: '5',
+      id: 5,
       firstName: 'Serena',
       location: 'San Francisco',
     },
   ];
   return arr;
-};
+}
