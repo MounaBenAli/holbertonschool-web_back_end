003b5f1c013b5d73eeb614234f33bb19fdcf46a8
MounaBenAli <benalimouna1986@gmail.com>
2022-11-22 15:14:03 +0100

    5. Typed Arrays

---
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,14 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+
+  if (buffer.byteLength === length) {
+    try {
+      view.setInt8(position, value);
+    } catch (e) {
+      throw Error('Position outside range');
+    }
+  }
+
+  return view;
+}
