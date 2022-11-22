4d21df73db2a449f10a132c19ce1da285eba560d
MounaBenAli <benalimouna1986@gmail.com>
2022-11-22 10:02:42 +0100

    3. Reduce

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,5 @@
 export default function getStudentIdsSum(arr) {
   const initialValue = 0;
-  const sum = (arr.reduce((accumulator, currentValue) => {
-    return currentValue.id + accumulator}, initialValue));
+  const sum = (arr.reduce((accum, currentValue) => currentValue.id + accum, initialValue));
   return sum;
-};
+}
