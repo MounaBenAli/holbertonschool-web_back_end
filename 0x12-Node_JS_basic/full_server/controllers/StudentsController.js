baf40df7c1348272a7b5c6bbb0957bb13fded3c5
MounaBenAli <benalimouna1986@gmail.com>
2022-11-28 16:00:42 +0100

    complex Http server with express

---
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,13 @@
+const readDatabase = require("../utils");
+
+class StudentsController {
+    static getAllStudents(request, response) {
+        return response.status(200);
+    }
+
+    static getAllStudentsByMajor(request, response) {
+        
+    }
+  }
+
+  module.exports = StudentsController;
