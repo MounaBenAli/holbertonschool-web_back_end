b5545a34b084aa9cfc245457b72a083631e9b7b6
MounaBenAli <benalimouna1986@gmail.com>
2022-11-28 10:25:39 +0100

    5. Create a more complex HTTP server using Node's HTTP module

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello Holberton School!');
+    res.end();
+  } else (req.url === '/students'); {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students');
+    res.end();
+  }
+});
+
+app.listen(port, hostname, () => {
+  //console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
