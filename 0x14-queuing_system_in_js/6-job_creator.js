c198f348ebfe5264cc227a0ec007887445372959
MounaBenAli <benalimouna1986@gmail.com>
2022-12-06 12:09:01 +0100

    6. Create the Job creator

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ const kue = require('kue');
 const queue = kue.createQueue();
 
 const object = {
-  phoneNumber: '',
-  message: '',
+  phoneNumber: '4153518780',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', object).save((err) => {
