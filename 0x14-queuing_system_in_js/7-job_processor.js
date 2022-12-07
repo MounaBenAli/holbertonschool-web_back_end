e15b41a01a1e87ca5fd10d59656e35191522793a
MounaBenAli <benalimouna1986@gmail.com>
2022-12-07 14:39:10 +0100

    9. Track progress and errors with Kue: Create the Job processor

--- 0x14-queuing_system_in_js/7-job_processor.js
+++ 0x14-queuing_system_in_js/7-job_processor.js
@@ -8,9 +8,7 @@ const blacklisted = [
 
 function sendNotification(phoneNumber, message, job, done) {
   job.progress(0, 100);
-  if (blacklisted.includes(phoneNumber)) {
-    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
-  }
+  if (blacklisted.includes(phoneNumber)) done(new Error(`Phone number ${phoneNumber} is blacklisted`));
 
   job.progress(50, 100);
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
@@ -18,6 +16,6 @@ function sendNotification(phoneNumber, message, job, done) {
 }
 
 queue.process('push_notification_code_2', 2, (job, done) => {
-  sendNotification(job.phoneNumber, job.message, job, done);
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
   done();
 });
