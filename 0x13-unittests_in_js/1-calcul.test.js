7a88f54006e82d26e54c80008bbdba1a6b5e7972
MounaBenAli <benalimouna1986@gmail.com>
2023-01-21 13:54:19 +0100

    test

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,8 +1,10 @@
+/* eslint-disable jest/expect-expect */
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber type == SUM', () => {
   it('should return the sum of the two numbers rounded to the nearest integer', () => {
+    expect.hasAssertions();
     assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
     assert.strictEqual(calculateNumber('SUM', 2, 3.7), 6);
     assert.strictEqual(calculateNumber('SUM', 1, -1), 0);
@@ -12,6 +14,7 @@ describe('calculateNumber type == SUM', () => {
 
 describe('calculateNumber type == SUBTRACT', () => {
   it('should return the difference of the two numbers rounded to the nearest integer', () => {
+    expect.hasAssertions();
     assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
     assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
     assert.strictEqual(calculateNumber('SUBTRACT', 0.4, 1.6), -2);
@@ -21,6 +24,7 @@ describe('calculateNumber type == SUBTRACT', () => {
 
 describe('calculateNumber type == DIVIDE', () => {
   it('should return the division of the two numbers rounded to the nearest integer', () => {
+    expect.hasAssertions();
     assert.strictEqual(calculateNumber('DIVIDE', 2, 2.5), 0.6666666666666666);
     assert.strictEqual(calculateNumber('DIVIDE', 0.0, 2), 0);
     assert.strictEqual(calculateNumber('DIVIDE', -1, 1), -1);
