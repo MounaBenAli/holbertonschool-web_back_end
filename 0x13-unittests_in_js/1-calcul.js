940040d192e32419063acd348966faddd0e86575
MounaBenAli <benalimouna1986@gmail.com>
2023-01-21 14:13:39 +0100

    Fixed a little bug...

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,14 +1,10 @@
 function calculateNumber(type, a, b) {
-  if (type === 'SUM') {
-    return Math.round(a) + Math.round(b);
-  } if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  } if (type === 'DIVIDE') {
-    if (b === 0) {
-      return 'Error';
-    }
-    return Math.round(a) / Math.round(b);
-  }
+  return type === 'SUM' 
+  ? Math.round(a) + Math.round(b) 
+  : type === 'SUBTRACT' 
+  ? Math.round(a) - Math.round(b) 
+  : type === 'DIVIDE' && Math.round(b) !== 0
+  ? Math.round(a) / Math.round(b)
+  : 'Error';
 }
-
 module.exports = calculateNumber;
