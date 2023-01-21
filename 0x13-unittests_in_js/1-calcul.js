3439130a2b06db632da9a8a2d372179f361694eb
MounaBenAli <benalimouna1986@gmail.com>
2023-01-21 14:10:51 +0100

    test

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,20 +1,14 @@
-module.exports = function calculateNumber(type, a, b) {
-  const numA = Number(a);
-  const numB = Number(b);
-
-  if (Number.isNaN(numA) || Number.isNaN(numB))
-  throw TypeError;
-
+function calculateNumber(type, a, b) {
   if (type === 'SUM') {
-    return (Math.round(numA) + Math.round(numB));
-  } else if (type === 'SUBTRACT') {
-    return (Math.round(numA) - Math.round(numB));
-  } else if (type === 'DIVIDE') {
-      if (Math.round(numB) === 0) {
-        return ('Error');
-      }
-      return (Math.round(numA) / Math.round(numB));
-  } else {
-    throw TypeError;
+    return Math.round(a) + Math.round(b);
+  } if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  } if (type === 'DIVIDE') {
+    if (b === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
   }
 }
+
+module.exports = calculateNumber;
